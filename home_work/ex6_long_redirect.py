142c4d52ea95b9439bed6d758217255aeba79457
sg <yavorskiy.dt@gmail.com>
2022-08-24 21:45:31 +0300

    Ex6: Длинный редирект. Доработка скрипта.

--- home_work/ex6_long_redirect.py
+++ home_work/ex6_long_redirect.py
@@ -2,13 +2,18 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect")
 
-response_list = response.history
+response_list_history = response.history
 
 urls_list = []
 i = 0
-while i < len(response_list):
-    urls_list.append(response_list[i].url)
+while i < len(response_list_history):
+    urls_list.append(response_list_history[i].url)
     i += 1
 
+urls_list.append(response.url)
+
 for x in urls_list:
+    if x is urls_list[-1]:
+        print(f"Last url: {x}")
+        break
     print(x)

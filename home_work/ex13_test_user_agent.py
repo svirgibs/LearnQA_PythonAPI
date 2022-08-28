7c214130cd5d01ef82b4482b5b53685fc6fe88c0
sg <yavorskiy.dt@gmail.com>
2022-08-28 17:59:31 +0300

    Ex13: User Agent

---
+++ home_work/ex13_test_user_agent.py
@@ -0,0 +1,27 @@
+import pytest
+import requests
+import json
+
+user_agents_list = [
+    ('Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) '
+    'Version/4.0 Mobile Safari/534.30', {'platform': 'Mobile', 'browser': 'No', 'device': 'Android'}),
+    ('Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 '
+    'Mobile/15E148 Safari/604.1', {'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'}),
+    ('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+     {'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'}),
+    ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
+    ' Edg/91.0.100.0', {'platform': 'Web', 'browser': 'Chrome', 'device': 'No'}),
+    ('Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 '
+    'Mobile/15E148 Safari/604.1', {'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'})
+]
+
+
+@pytest.mark.parametrize('user_agent, expected_values', user_agents_list)
+class TestUserAgent:
+    def test_user_agent(self, user_agent, expected_values):
+        response = requests.get("https://playground.learnqa.ru/ajax/api/user_agent_check",
+                                headers={'User-agent': user_agent})
+        response_dict = json.loads(response.text)
+        assert response_dict['platform'] == expected_values['platform']
+        assert response_dict['browser'] == expected_values['browser']
+        assert response_dict['device'] == expected_values['device']

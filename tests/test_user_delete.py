8d831f3b29c9ae4deabd5c70d6b4b676a14c15df
sg <yavorskiy.dt@gmail.com>
2022-09-10 19:33:46 +0300

    Ex18: Тесты на DELETE (part 2)

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -56,3 +56,40 @@ class TestUserDelete(BaseCase):
         Assertions.assert_code_status(response4, 404)
         assert response4.content.decode("utf-8") == f"User not found", \
             f"Unexpected response content {response4.content}"
+
+    def test_delete_another_user(self):
+        # Register 1
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        password = register_data['password']
+
+        # Register 2
+        register_data = self.prepare_registration_data()
+        response2 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response2, 200)
+        Assertions.assert_json_has_key(response2, "id")
+
+        user_id_2 = self.get_json_value(response2, "id")
+
+        # Login
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response3 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response3, "auth_sid")
+        token = self.get_header(response3, "x-csrf-token")
+
+        # Delete
+        response4 = MyRequests.delete(f"/user/{user_id_2}",
+                                      headers={"x-csrf-token": token},
+                                      cookies={"auth_sid": auth_sid})
+        Assertions.assert_code_status(response4, 200)
+        assert response4.content.decode("utf-8") != f"User not found", \
+            f"Unexpected response content {response4.content}"

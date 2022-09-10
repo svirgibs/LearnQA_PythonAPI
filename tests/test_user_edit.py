160ee5001ee9f25de88513625d9981e8b5417a74
sg <yavorskiy.dt@gmail.com>
2022-09-10 16:18:35 +0300

    Ex17: Негативные тесты на PUT - Попытаемся изменить данные пользователя, будучи неавторизованными

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -39,3 +39,21 @@ class TestUserEdit(BaseCase):
                                    cookies={"auth_sid": auth_sid})
         Assertions.assert_json_value_by_name(response4, "firstName", new_name,
                                              "Wrong name of the user after edit")
+
+    def test_edit_without_token_and_auth_sid(self):
+        # Register
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        user_id = self.get_json_value(response1, "id")
+
+        # Edit
+        new_name = "Changed Name"
+        response2 = MyRequests.put(f"/user/{user_id}", data={"firstName": new_name})
+        Assertions.assert_code_status(response2, 400)
+        assert response2.content.decode("utf-8") == f"Auth token not supplied", \
+            f"Unexpected response content {response2.content}"
+
